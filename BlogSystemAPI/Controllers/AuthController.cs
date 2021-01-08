f53014ed7ee49b888e0fd4d860c26883c7bae0fa
Vural Vardarelli <vuralvardarelli@hotmail.com>
2021-01-08 21:44:58 +0300

    authentication renewed

---
+++ BlogSystemAPI/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using BlogSystemAPI.Models.Database;
+using BlogSystemAPI.Services.UserService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogSystemAPI.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public AuthController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] User userParam)
+        {
+            var user = _userService.Authenticate(userParam.Username, userParam.Password);
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(user);
+        }
+    }
+}
--- BlogSystemAPI/Controllers/BlogController.cs
+++ BlogSystemAPI/Controllers/BlogController.cs
@@ -14,24 +14,6 @@ namespace BlogSystemAPI.Controllers
     [Route("api/[controller]")]
     public class BlogController : ControllerBase
     {
-        private IUserService _userService;
-
-        public BlogController(IUserService userService)
-        {
-            _userService = userService;
-        }
-
-        [AllowAnonymous]
-        [HttpPost("authenticate")]
-        public IActionResult Authenticate([FromBody] User userParam)
-        {
-            var user = _userService.Authenticate(userParam.Username, userParam.Password);
-            if (user == null)
-                return BadRequest(new { message = "Username or password is incorrect" });
-
-            return Ok(user);
-        }
-
         [HttpGet("Test")]
         public async Task<string> Test()
         {
